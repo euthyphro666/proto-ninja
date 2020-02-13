2bfdc1bea035113b9434b6d76c3e65430c95649d
Aidan <Aidan.j.oconnell@gmail.com>
2020-02-12 20:30:32 -0700

    Wasp
    Added wasp with basic movement

---
+++ Assets/Scripts/Wasp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Wasp : MonoBehaviour
+
+{
+
+    public Transform Player;
+    public int Movespeed;
+    public int MaxDist;
+    public int MinDist;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.Translate(transform.forward * Movespeed * Time.deltaTime);
+
+
+            if(Vector3.Distance(transform.position, transform.position) <= MaxDist)
+            {
+
+            }
+        }
+
+        
+    }
+}
