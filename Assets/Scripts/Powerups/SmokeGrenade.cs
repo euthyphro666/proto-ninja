732eb7f90b4f2d1adb6f4adb52208c9ff14171d6
Jared Ross <jdross91@gmail.com>
2020-02-19 22:04:02 -0800

    Auto stash before merge of "master" and "origin/master" Add Smoke grenade

---
+++ Assets/Scripts/Powerups/SmokeGrenade.cs
@@ -0,0 +1,34 @@
+﻿using SomethingSpecific.ProtoNinja;
+using UnityEngine;
+
+public class SmokeGrenade : MonoBehaviour, IPowerup
+{
+    
+    public Player OwningPlayer { get; set; }
+
+    [SerializeField] private GameObject smokeEffectPrefab;
+    [SerializeField] private float smokeDurationInSeconds = 3;
+
+    private float createTime;
+    private float currentTime;
+    private GameObject smokeEffect;
+    
+    void Start()
+    {
+        // start the smoke animation
+        smokeEffect = Instantiate(smokeEffectPrefab, gameObject.transform.position + new Vector3(0, 3, 0), Quaternion.identity);
+        createTime = currentTime = Time.deltaTime;
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        
+        if (currentTime - createTime > smokeDurationInSeconds)
+        {
+            Debug.Log("destroying smoke");
+            Destroy(smokeEffect);
+            Destroy(gameObject);
+        }
+    }
+}
