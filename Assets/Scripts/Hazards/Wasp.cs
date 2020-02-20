2d0318df44114f5bf41ee655442eb9ac31da4c26
Josh <joshhess13@gmail.com>
2020-02-19 21:14:25 -0700

    Added bear trap and more folders

---
+++ Assets/Scripts/Hazards/Wasp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Wasp : MonoBehaviour
+
+{
+
+    public Transform Player;
+    public int Movespeed;
+    public int MaxDist;
+    public int MinDist;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            Debug.DrawRay(transform.position, transform.forward * Movespeed, Color.magenta, Time.deltaTime);
+            transform.Translate(transform.forward * Movespeed * Time.deltaTime, Space.World);
+
+
+            if (Vector3.Distance(transform.position, transform.position) <= MaxDist)
+            {
+
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Powerups/BearTrap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SomethingSpecific.ProtoNinja
+{
+    public class BearTrap : MonoBehaviour
+    {
+        public float FreezeTime = 0.5f;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(typeof(Player), out var comp) &&
+                comp is Player player)
+            {
+                player.Freeze(FreezeTime);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
